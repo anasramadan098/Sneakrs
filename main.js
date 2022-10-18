8e7d0c5539edb838155e18db68b99b23dd6bcf40
Anas Ramadan <99337703+anasramadan098@users.noreply.github.com>
2022-10-18 20:56:15 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 let images = document.querySelectorAll(".other-img img");
 let myimg = document.querySelector(".myimg");
 let myimgtwo = document.querySelector(".myimgtwo");
+let cartICon = document.querySelector('.cart')
+let cartIConImg = document.querySelector('.cart img')
+let item = document.querySelector('.cart .numbers');
+let itemsNumbers;
+if (localStorage.getItem('Cart-Products')) {
+    itemsNumbers = localStorage.getItem('Cart-Products')
+}
+item.textContent = itemsNumbers 
+
+cartIConImg.addEventListener('click', function () {
+    cartICon.classList.toggle('active')
+})
 images.forEach((e)=> {
     let myattr = e.getAttribute('data-src')
     e.addEventListener('click',(e)=>{
         myimg.src = myattr
         myimgtwo.src = myattr
     } )
-})
+}) 
+//
 myimg.onclick = function() {
     document.querySelector('.max-show').classList.add('active')
 }
@@ -20,7 +33,8 @@ document.querySelector('.close').onclick = function() {
 function addToCart() {
     let productprice = 125.00;
     let myValue = document.querySelector('#num').value;
-    let card = document.createElement("div");
+    // Create Cart Items
+    let card = document.createElement("div");  
     let deleteIcon = document.createElement("img");
     deleteIcon.setAttribute('src','images/icon-delete.svg');
     deleteIcon.classList.add('deleteicon');
@@ -43,16 +57,21 @@ function addToCart() {
     let ontherspan = document.createElement('span');
     ontherspan.textContent = total;
     ontherspan.classList.add('totalpricee')
+    // Append
     contentdiv.append(span);
     contentdiv.append(ontherspan);
     card.append(contentdiv);
-    card.append(deleteIcon)
-    document.querySelector('.card-menu .cont').append(card)
-    let allholders = document.querySelectorAll(".card-menu .cont .holder")
-    deleteIcon.addEventListener('click',()=> {
-        card.remove();
-        document.querySelector(".cart").setAttribute("data-value" , allholders.length);
-    })
-    document.querySelector(".cart").setAttribute("data-value" , allholders.length);
-}
-document.querySelector('.add').addEventListener('click' , addToCart)
+    card.append(deleteIcon);
+        deleteIcon.addEventListener('click',()=> {
+                card.remove()
+                itemsNumbers = document.querySelectorAll('.card-menu .cont .holder').length;
+                item.textContent = itemsNumbers 
+                localStorage.setItem('Cart-Products',itemsNumbers);
+        })
+    // Values
+    document.querySelector('.card-menu .cont').append(card);
+    itemsNumbers = document.querySelectorAll('.card-menu .cont .holder').length;
+    item.textContent = itemsNumbers 
+    localStorage.setItem('Cart-Products',itemsNumbers); 
+} 
+document.querySelector('.add').addEventListener('click' , addToCart) 
